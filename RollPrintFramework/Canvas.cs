96be4d31a0b80b7db46f93479e4fc21df966cef8
Crafthero2000 <misha.kurtukov2004@gmail.com>
2024-06-24 18:48:50 +0300

    Добавлен пользовательский контрол Canvas

---
+++ RollPrintFramework/Canvas.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace RollPrintFramework
+{
+    public partial class Canvas : PictureBox
+    {
+        //Image changed delegate
+        public delegate void ImageChanged(Object sender, EventArgs e);
+
+        //event raised when image changed
+        public event ImageChanged? OnImageChange;
+
+        //overridden/hidden Image property with event hooked up
+        public new Image Image
+        {
+            get { return base.Image; }
+            set
+            {
+                base.Image = value;
+                if (OnImageChange != null)
+                {
+                    OnImageChange(this, new EventArgs());
+                }
+            }
+        }
+    }
+}
