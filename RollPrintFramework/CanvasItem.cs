63594a427f2379b37924d4baae24f954884e02b7
Crafthero2000 <misha.kurtukov2004@gmail.com>
2024-06-24 18:49:07 +0300

    Добавлен CanvasItem

---
+++ RollPrintFramework/CanvasItem.cs
@@ -0,0 +1,16 @@
+﻿using System.Drawing;
+
+namespace RollPrintFramework
+{
+    public class CanvasItem
+    {
+        public int Position;
+        public string Type;
+        public Bitmap Bitmap = new Bitmap(1, 1);
+        public string HeaderText;
+        public string DescriptionText;
+        public Color BackColor;
+        public virtual void Draw(int upperMargin = 0) { }
+        public void GenInfoText(string otherData) { HeaderText = $"{Position} | {Type}"; DescriptionText = otherData; }
+    }
+}
